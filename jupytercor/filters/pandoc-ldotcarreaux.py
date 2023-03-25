2cc2530d78c71d6abc70cb3a7cd6a0f4a19a0ab3
infocornouaille <couronne.david@gmail.com>
2023-03-25 21:31:18 +0100

    Update pandoc-ldotcarreaux.py

--- jupytercor/filters/pandoc-ldotcarreaux.py
+++ jupytercor/filters/pandoc-ldotcarreaux.py
@@ -1,15 +1,41 @@
 #!/usr/bin/env python
+# coding: utf-8
 """ A pandoc filter that convert blank lines in ldotcarreaux
 Usage:
     pandoc --filter ./ldotcarreaux.py -o myfile.tex myfile.md
 """
 
+""" Here is the explanation for the code above:
+1. We import the pandocfilters library and the Template class from the string library.
+2. We define the function is_empty_cell. It takes a list of strings and returns a Boolean value.
+3. We define the function unpack_code. It takes a dictionary and a string as arguments.
+4. We define the function unpack_metadata. It takes a dictionary as argument.
+5. We define the function ldotcarreaux. It takes four arguments: key, value, format, and meta.
+6. We define a template. It is a string with a placeholder $longueur. The value of the placeholder will be replaced by the value of the key longueur in the dictionary code.
+7. We define the function ldotcarreaux. It takes four arguments: key, value, format, and meta.
+8. We test if the output format is latex.
+9. We test if the key is CodeBlock.
+10. We unpack the metadata.
+11. We unpack the code.
+12. We test if the code is empty. If it is, we return a list with a RawBlock element. Otherwise, we return nothing. """
+
+
+""" This is an example for calling this function: 
+import ldotcarreaux
+ldotcarreaux.ldotcarreaux("CodeBlock", ["", ["text"], []], "latex", None)
+
+It should return:
+
+[RawBlock('latex', '\\ldotcarreaux[0]')] """
+
+
 from string import Template
 from typing import Dict, List
-from pandocfilters import toJSONFilter, RawBlock, RawInline
+
+from pandocfilters import RawBlock, toJSONFilter
 
 
-def est_vide_cell(source: List[str]) -> bool:
+def is_empty_cell(source: List[str]) -> bool:
     """Determine if a cell is empty or contains only blank lines."""
     for elem in source:
         if elem != "" and elem != "\n":
@@ -40,7 +66,7 @@ def unpack_code(value: Dict, language: str) -> Dict:
         "attributes": attributes,
         "lines": lines,
         "longueur": len(lines),
-        "estvide": est_vide_cell(contents),
+        "estvide": is_empty_cell(contents),
         "message": "truc",
     }
 
@@ -92,7 +118,7 @@ def ldotcarreaux(key: str, value: str, format: str, meta: dict) -> list:
 
     code = unpack_code(value, settings["language"])
 
-    # If the code is empty, return the ldotcarreaux
+    # If the code is empty, return None.
     if not code["estvide"]:
         return
 
