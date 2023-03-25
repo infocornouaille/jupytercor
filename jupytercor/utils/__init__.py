cdc6b57c83f9d86f13e23e2640ad6f109625acd4
infocornouaille <couronne.david@gmail.com>
2023-03-25 22:08:56 +0100

    Update __init__.py

--- jupytercor/utils/__init__.py
+++ jupytercor/utils/__init__.py
@@ -1,20 +1,31 @@
-from urllib.parse import urlparse
 import subprocess
+from urllib.parse import urlparse
+
 import nbformat
 
 
-def is_valid_url(url):
-    # Parse l'url en ses composants
+def is_valid_url(url: str) -> bool:
+    """Check if the url is valid
+    Args:
+        url (str): The url to check
+
+        Returns:
+            bool: True if the url is valid, False otherwise"""
     result = urlparse(url)
-    # Retourne True si le schéma est http ou https
+    # Check if the scheme is http or https
     return result.scheme in ("http", "https")
 
 
-def clean_markdown(nb: nbformat):
-    """Read the input notebook and convert all markdown cells
-    into a clean markdown without html tags"""
-    # Loop through the cells and transform markdown cells with pandoc if clean is True
+def clean_markdown(nb: nbformat) -> nbformat:
+    """Clean the markdown cells with pandoc conversions
+    Read the input notebook and convert all markdown cells into a clean markdown without html tags.
 
+    Args:
+        nb (nbformat): The notebook to clean
+
+        Returns:
+            nbformat: The cleaned notebook"""
+    # Loop through the cells and transform markdown cells with pandoc if clean is True
     for cell in nb.cells:
         if cell.cell_type == "markdown":
             # Run a pandoc command to convert markdown to html
@@ -23,11 +34,14 @@ def clean_markdown(nb: nbformat):
                 input=cell.source.encode(),
                 capture_output=True,
             )
+
+            # Run a pandoc command to convert html to markdown
             result = subprocess.run(
                 ["pandoc", "-f", "html", "-t", "gfm-raw_html", "-o", "-"],
                 input=html.stdout,
                 capture_output=True,
             )
+
             # Replace the cell source with the transformed text
             cell.source = result.stdout.decode()
 
